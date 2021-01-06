34ff506bbac41676eea3f842e9fc5bc1c67aa45d
marcuspgraham <64594348+marcuspgraham@users.noreply.github.com>
2021-01-06 18:40:05 +0000

    Sarah is a git guru

---
+++ app.js
@@ -0,0 +1,19 @@
+const modalWrap = document.querySelector(".modal-wrap");
+const modalContent = document.querySelector(".modal-content");
+const modalBtn = document.querySelector(".modal-btn");
+const cancel = document.querySelector(".accept");
+
+
+modalBtn.addEventListener("click", () => {
+    modalContent.classList.remove("slide-out");
+    modalWrap.classList.toggle("display-none");
+});
+
+
+cancel.addEventListener("click", () => {
+    modalContent.classList.add("slide-out");
+    setTimeout(() => {
+        modalWrap.classList.toggle("display-none");
+    }, 650)
+});
+
